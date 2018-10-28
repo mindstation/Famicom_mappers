240efd26f9f466246ddada3fe10317e93a54fd88
Alexander Kirichenko <aakirichenko@gmail.com>
2018-10-28 20:14:07 +0500

    Rewrote a power-on state.
    Made zero power-on state registers be separated from one power-on state.

--- MMC1proj/MMC1_quartus/wholeMMC1.v
+++ MMC1proj/MMC1_quartus/wholeMMC1.v
@@ -25,11 +25,14 @@ module wholeMMC1 (
 						);
 						
 	//MMC1 registers
-	reg[4:0] rLoad = 5'b10000; 		//MMC1 Load shift register with power on reset state.
-	reg[4:0] rControl = 5'b01100; 	//MMC1 Control register with power on reset state.
+	reg[4:4] rLoad4 = 1'b1;
+	reg[3:0] rLoad;				 		//MMC1 Load shift register (rLoad4 + rLoad) with power on reset state 5'b10000.
+	reg[1:0] rControl32 = 2'b11;
+	reg[2:0] rControl; 					//MMC1 Control register (rControl32 + rControl) with power on reset state 5'b01100.
+	reg[4:0] rPRG_b; 						//MMC1 PRG bank selector. With default power on reset state (all zeros) like MMC1C version.
 	reg[4:0] rCHR_b0; 					//MMC1 CHR bank 0 selector.
-	reg[4:0] rCHR_b1; 					//MMC1 CHR bank 1 selector.
-	reg[4:0] rPRG_b = 5'b00000; 		//MMC1 PRG bank selector with power on reset state like MMC1C version.
+	reg[4:0] rCHR_b1; 					//MMC1 CHR bank 1 selector. CHR is zero too, because it's default power on.
+	
 
 	//Output registers
 	reg[3:0] oPRG_A; //Extended program ROM address.
@@ -52,7 +55,7 @@ module wholeMMC1 (
 	
 	//CPU and PPU works with different clocks. Made a combinational logic (multiplexer) for the PPU bus.
 	//A part of the CHR ROM bank switching mode. Multiplexer3.
-	assign CHR_A12 = rControl[4] ? (PPU_A12 ? rCHR_b1[0] : rCHR_b0[0]) : PPU_A12;
+	assign CHR_A12 = rControl[2] ? (PPU_A12 ? rCHR_b1[0] : rCHR_b0[0]) : PPU_A12;
 		//It looks like a short circuit, if MMC1 CHR_A12 connected to ROM with PPU_A12. DON'T DO IT!
 	
 	//Mirroring mode. Multiplexer4. 00 - One-screen Low. 01 - One-screen High. 10 - Two-screen vertical. 11 - Two-screen horizontal.
@@ -66,25 +69,34 @@ module wholeMMC1 (
 				begin
 					if (CPU_D7)
 						begin
-							rLoad = 5'b10000; // The initial value.
-							rControl = rControl || 5'b01100; //fixed last PRG bank at $C000, don't change other bits.
+							rLoad4 = 1'b1;
+							rLoad = 4'b0000; // The initial value.
+							
+							rControl32 = 2'b11; //fixed last PRG bank at $C000, don't change other bits.
 						end
 					else
 						begin
 							if (rLoad[0]) //Inintial 1 come to a zero position, 4 writes was made.
 								begin
 									case ({CPU_A14, CPU_A13})
-										2'b00: rControl = {CPU_D0,rLoad[4:1]};
-										2'b01: rCHR_b0 = {CPU_D0,rLoad[4:1]};
-										2'b10: rCHR_b1 = {CPU_D0,rLoad[4:1]};
-										2'b11: rPRG_b = {CPU_D0,rLoad[4:1]};
+										2'b00:
+											begin
+												rControl = {CPU_D0,rLoad[2:1]};
+												rControl32 = {rLoad4[4],rLoad[3]};
+											end
+										2'b01: rCHR_b0 = {CPU_D0,rLoad4[4],rLoad[3:1]};
+										2'b10: rCHR_b1 = {CPU_D0,rLoad4[4],rLoad[3:1]};
+										2'b11: rPRG_b = {CPU_D0,rLoad4[4],rLoad[3:1]};
 									endcase
-									rLoad = 5'b10000; // Reset to inintial value
+									
+									rLoad4 = 1'b1;
+									rLoad = 4'b0000; // Reset to inintial value
 								end
 							else
 								begin
 									rLoad = rLoad >> 1'd1;
-									rLoad[4] = CPU_D0;
+									rLoad[3] = rLoad4;
+									rLoad4 = CPU_D0;
 								end
 						end
 				end
@@ -93,8 +105,8 @@ module wholeMMC1 (
 	always //Switching out async nCPU_ROMSEL
 		begin
 			//PRG ROM bank switching mode.
-			if (rControl[3]) //2'b10, 2'b11:
-				if (rControl[3] && rControl[2]) //2'b11
+			if (rControl32[1]) //2'b10, 2'b11:
+				if (rControl32[1] && rControl32[0]) //2'b11
 					begin //Fix last bank at $C000 (CPU_A14 is high) and switch 16 KB bank at $8000 (CPU_A14 is low).
 						oPRG_A[0] = rPRG_b[0] || CPU_A14;
 						oPRG_A[1] = rPRG_b[1] || CPU_A14;
@@ -111,10 +123,10 @@ module wholeMMC1 (
 			else //2'b00, 2'b01:
 				begin //Switch 32 KB at $8000.
 					oPRG_A[3:1] = rPRG_b[3:1];
-					oPRG_A[0] = rControl[3] && CPU_A14;
+					oPRG_A[0] = rControl32[1] && CPU_A14;
 				end
 					
-			if (rControl[4]) //CHR ROM bank switching mode.
+			if (rControl[2]) //CHR ROM bank switching mode.
 				begin //If 1 then switch two separate 4 KB banks.
 					//If the same value is in both CHR registers, 4KB mode causes erratic switching of bank
 					//during rendering.
